9732ad3137de20e587a8980e53412aee713811a5
202110861 <dbsalsk2514@naver.com>
2025-10-29 15:48:57 +0900

    feat(Repeat) : OutputView 클래스 함수 적용

--- src/Repeat.js
+++ src/Repeat.js
@@ -1,10 +1,12 @@
 import Lotto from "./Lotto.js";
 import { Random } from "@woowacourse/mission-utils";
+import { OutputView } from "./view/OutputView.js";
 
 export const Repeat = (count) => {
+  const outputView = new OutputView();
   for (let i = 0; i < count; i++) {
     const numberList = Random.pickUniqueNumbersInRange(1, 45, 6);
-
+    outputView.printNumberList(numberList);
     const lotto = new Lotto(numberList);
   }
 };
