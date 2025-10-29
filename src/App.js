f66435be668a5ad46183d5abc9f8cd89e661bb34
202110861 <dbsalsk2514@naver.com>
2025-10-29 14:25:06 +0900

    feat : 구매 개수 계산하는 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { Console } from "@woowacourse/mission-utils";
 import { INPUT_MESSAGE } from "./Constants.js";
+import { CalculatePurchaseCount } from "./CalculatePurchaseCount.js";
 class App {
   async run() {
     const purchaseAmount = Console.readLineAsync(INPUT_MESSAGE.BONUS_NUMBER);
+    const count = CalculatePurchaseCount(await purchaseAmount);
   }
 }
 
---
+++ src/CalculatePurchaseCount.js
@@ -0,0 +1,4 @@
+export const CalculatePurchaseCount = (amount) => {
+  const count = Number(amount) / 1000;
+  return count;
+};
