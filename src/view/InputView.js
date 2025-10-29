852feded9e939fcc8e825b05855a8df9ce185772
202110861 <dbsalsk2514@naver.com>
2025-10-29 14:31:59 +0900

    feat : input 코드 클래스 추가

---
+++ src/view/InputView.js
@@ -0,0 +1,20 @@
+import { Console } from "@woowacourse/mission-utils";
+import { INPUT_MESSAGE } from "../Constants";
+
+export class InputView {
+  async inputPurchase() {
+    const input = await Console.readLineAsync(INPUT_MESSAGE.PURCHASE_AMOUNT);
+
+    return input;
+  }
+
+  async inputWinningNumber() {
+    const input = await Console.readLineAsync(INPUT_MESSAGE.WINNING_NUMBERS);
+    return input;
+  }
+
+  async inputBonusNumber() {
+    const input = await Console.readLineAsync(INPUT_MESSAGE.BONUS_NUMBER);
+    return input;
+  }
+}
