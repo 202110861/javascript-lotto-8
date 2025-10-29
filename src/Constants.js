8f5abff340eefbadf6c6df84e8a8bde4ca716509
202110861 <dbsalsk2514@naver.com>
2025-10-29 14:44:27 +0900

    refactor : output 메세지 상수화

--- src/Constants.js
+++ src/Constants.js
@@ -3,3 +3,8 @@ export const INPUT_MESSAGE = {
   WINNING_NUMBERS: "당첨 번호를 입력해 주세요.",
   BONUS_NUMBER: "보너스 번호를 입력해 주세요",
 };
+
+export const OUTPUT_MESSAGE = {
+  PURCHASE_COUNT: "개를 구매했습니다.",
+  RESULT_TITLE: "당첨 통계\n---",
+};
