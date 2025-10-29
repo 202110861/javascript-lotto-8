c9f73f2d1c729451ba785bd620b207bb9a291453
202110861 <dbsalsk2514@naver.com>
2025-10-29 14:00:07 +0900

    feat : 구입금액 입력 코드 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import { Console } from "@woowacourse/mission-utils";
+import { INPUT_MESSAGE } from "./Constants.js";
 class App {
-  async run() {}
+  async run() {
+    const purchaseAmount = Console.readLineAsync(INPUT_MESSAGE.BONUS_NUMBER);
+  }
 }
 
 export default App;
