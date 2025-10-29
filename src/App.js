5c45dcc9812231ee4063e65d0c9612de260440f8
202110861 <dbsalsk2514@naver.com>
2025-10-29 15:03:02 +0900

    feat(app.js) outputView 코드 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { InputView } from "./view/InputView.js";
 import { CalculatePurchaseCount } from "./CalculatePurchaseCount.js";
+import { OutputView } from "./view/OutputView.js";
 class App {
   async run() {
     const inputView = new InputView();
+    const outputView = new OutputView();
+
     const purchaseAmount = inputView.inputPurchase();
     const count = CalculatePurchaseCount(await purchaseAmount);
+    outputView.printPurchaseCount(count);
   }
 }
 
