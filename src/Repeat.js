50f2ad1cbfde98156bc1e0dfa62e343e615def4a
202110861 <dbsalsk2514@naver.com>
2025-10-29 16:19:22 +0900

    feat(Repeat) : 정렬된 로또번호를 객체에 추가하여 반환

--- src/Repeat.js
+++ src/Repeat.js
@@ -4,11 +4,15 @@ import { OutputView } from "./view/OutputView.js";
 
 export const Repeat = (count) => {
   const outputView = new OutputView();
+  const lottoNumberList = [];
   for (let i = 0; i < count; i++) {
     const numberList = Random.pickUniqueNumbersInRange(1, 45, 6);
 
     const lotto = new Lotto(numberList);
     const sortedNumber = lotto.getNumbers(numberList);
     outputView.printNumberList(sortedNumber);
+    lottoNumberList.push(sortedNumber);
   }
+
+  return lottoNumberList;
 };
