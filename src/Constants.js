d1b9371058701270369ec5de0f6a4f12dd68260d
202110861 <dbsalsk2514@naver.com>
2025-10-30 14:51:38 +0900

    refactor : 일치하는 개수별 당첨 금액 상수 이름 변경

--- src/Constants.js
+++ src/Constants.js
@@ -1,17 +1,17 @@
 export const INPUT_MESSAGE = {
-  PURCHASE_AMOUNT: "구입금액을 입력해 주세요.",
-  WINNING_NUMBERS: "당첨 번호를 입력해 주세요.",
-  BONUS_NUMBER: "보너스 번호를 입력해 주세요",
+  PURCHASE_AMOUNT: "구입금액을 입력해 주세요.\n",
+  WINNING_NUMBERS: "당첨 번호를 입력해 주세요.\n",
+  BONUS_NUMBER: "보너스 번호를 입력해 주세요.\n",
 };
 
 export const OUTPUT_MESSAGE = {
   PURCHASE_COUNT: "개를 구매했습니다.",
   RESULT_TITLE: "당첨 통계\n---",
   RESULT_CONTENT: {
-    THREE: { 3: "5,000원" },
-    FORE: { 4: "50,000원" },
-    FIVE: { 5: "1,500,000원" },
-    FIVE_BONUS: { 5: "30,000,000원" },
-    SIX: { 6: "2,000,000,000원" },
+    3: "5,000원",
+    4: "50,000원",
+    5: "1,500,000원",
+    "5_BONUS": "30,000,000원",
+    6: "2,000,000,000원",
   },
 };
