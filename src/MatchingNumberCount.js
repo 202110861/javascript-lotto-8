a41092a4113fb4d5a55dbb4d0094c1582e06661d
202110861 <dbsalsk2514@naver.com>
2025-10-30 19:47:41 +0900

    feat : 로또번호 문자열을 배열로 변경하는 함수 추가

--- src/MatchingNumberCount.js
+++ src/MatchingNumberCount.js
@@ -20,18 +20,29 @@ export const MatchingNumberCount = (
 
 const matchingNumber = (lotto, winningNumber) => {
   let count = 0;
+  const lottoArray = parseLottoStringToArray(lotto);
   const winningNumberArray = winningNumber.split(",");
   winningNumberArray.forEach((num) => {
     const number = Number(num);
-    if (lotto.includes(number)) {
+    if (lottoArray.includes(number)) {
       count++;
     }
   });
   return count;
 };
 
+const parseLottoStringToArray = (lottoString) => {
+  return lottoString
+    .replace("[", "")
+    .replace("]", "")
+    .split(", ")
+    .map((s) => Number(s.trim()))
+    .filter((v) => !isNaN(v));
+};
+
 export const checkBonusNumber = (lotto, bonusNumber) => {
   const number = Number(bonusNumber);
-  const isBonusNumber = lotto.includes(number);
+  const lottoArray = parseLottoStringToArray(lotto);
+  const isBonusNumber = lottoArray.includes(number);
   return isBonusNumber;
 };
