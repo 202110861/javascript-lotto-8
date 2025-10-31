f36393e713b214f18057b8205d117cca00cc4cc5
202110861 <dbsalsk2514@naver.com>
2025-10-31 19:31:13 +0900

    refactor : 로또컨트롤러 클래스 사용

--- src/App.js
+++ src/App.js
@@ -1,32 +1,9 @@
-import { InputView } from "./view/InputView.js";
-import { CalculatePurchaseCount } from "./CalculatePurchaseCount.js";
-import { OutputView } from "./view/OutputView.js";
-import { Repeat } from "./Repeat.js";
-import { CalculateResultCount } from "./CalculateResultCount.js";
-import { validateBonusNumber } from "./Validator.js";
+import { LottoController } from "./controller/LottoController";
+
 class App {
   async run() {
-    const inputView = new InputView();
-    const outputView = new OutputView();
-
-    const purchaseAmount = await inputView.inputPurchase();
-    const count = CalculatePurchaseCount(purchaseAmount);
-    outputView.printPurchaseCount(count);
-
-    const lottoList = Repeat(count);
-
-    const winningNumber = await inputView.inputWinningNumber();
-    const bonusNumber = await inputView.inputBonusNumber();
-    validateBonusNumber(bonusNumber, winningNumber);
-
-    outputView.printResultTitle();
-
-    const totalMoney = CalculateResultCount(
-      lottoList,
-      winningNumber,
-      bonusNumber
-    );
-    outputView.printProfitRate(totalMoney, purchaseAmount);
+    const controller = new LottoController();
+    await controller.play();
   }
 }
 
