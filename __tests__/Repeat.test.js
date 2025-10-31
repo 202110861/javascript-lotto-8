fdafb7c766a5965e3984d1d8e0a07c299b6c0eaa
202110861 <dbsalsk2514@naver.com>
2025-10-31 22:44:59 +0900

    feat : Repeat 테스트 코드 작성

---
+++ __tests__/Repeat.test.js
@@ -0,0 +1,44 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import { Repeat } from "../src/Repeat.js";
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickUniqueNumbersInRange);
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe("Repeat 테스트", () => {
+  test("로또 번호를 출력한다.", () => {
+    const logSpy = getLogSpy();
+    mockRandoms([
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44],
+      [1, 8, 11, 31, 41, 42],
+      [13, 14, 16, 38, 42, 45],
+      [7, 11, 30, 40, 42, 43],
+      [2, 13, 22, 32, 38, 45],
+      [1, 3, 5, 14, 22, 45],
+    ]);
+
+    const result = Repeat(8);
+    const logs = [
+      "[8, 21, 23, 41, 42, 43]",
+      "[3, 5, 11, 16, 32, 38]",
+      "[7, 11, 16, 35, 36, 44]",
+      "[1, 8, 11, 31, 41, 42]",
+      "[13, 14, 16, 38, 42, 45]",
+      "[7, 11, 30, 40, 42, 43]",
+      "[2, 13, 22, 32, 38, 45]",
+      "[1, 3, 5, 14, 22, 45]",
+    ];
+    expect(result).toEqual(logs);
+  });
+});
