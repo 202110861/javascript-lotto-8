16063eaee4e11e40f41a0514ee676e2e0288eed4
202110861 <dbsalsk2514@naver.com>
2025-11-01 01:31:31 +0900

    feat : CalculateResultCount 테스트 코드 작성

---
+++ __tests__/CalculateResultCount.test.js
@@ -0,0 +1,40 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import { CalculateResultCount } from "../src/CalculateResultCount.js";
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe("CalculateResultCount 테스트", () => {
+  test("등수별 개수를 집계하여 총 당청금을 계산한다.", () => {
+    const lottoList = [
+      "[8, 21, 23, 41, 42, 43]",
+      "[3, 5, 11, 16, 32, 38]",
+      "[7, 11, 16, 35, 36, 44]",
+      "[1, 8, 11, 31, 41, 42]",
+      "[13, 14, 16, 38, 42, 45]",
+      "[7, 11, 30, 40, 42, 43]",
+      "[2, 13, 22, 32, 38, 45]",
+      "[1, 3, 5, 14, 22, 45]",
+    ];
+    const winningNumber = "1,2,3,4,5,6";
+    const bonusNumber = "7";
+    const logSpy = getLogSpy();
+    const result = CalculateResultCount(lottoList, winningNumber, bonusNumber);
+
+    const logs = [
+      "3개 일치 (5,000원) - 1개",
+      "4개 일치 (50,000원) - 0개",
+      "5개 일치 (1,500,000원) - 0개",
+      "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+      "6개 일치 (2,000,000,000원) - 0개",
+    ];
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+
+    expect(result).toBe(5000);
+  });
+});
