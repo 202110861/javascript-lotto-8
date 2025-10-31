6a3ea078d85c3ca2cddb7e7ae4842e7824a57b46
202110861 <dbsalsk2514@naver.com>
2025-10-31 19:30:17 +0900

    refactor : MVC 패턴 적용하여 폴더 분리

---
+++ src/controller/LottoController.js
@@ -0,0 +1,32 @@
+import { InputView } from "../view/InputView.js";
+import { CalculatePurchaseCount } from "../CalculatePurchaseCount.js";
+import { OutputView } from "../view/OutputView.js";
+import { Repeat } from "../Repeat.js";
+import { CalculateResultCount } from "../CalculateResultCount.js";
+import { validateBonusNumber } from "../Validator.js";
+
+export class LottoController {
+  async play() {
+    const inputView = new InputView();
+    const outputView = new OutputView();
+
+    const purchaseAmount = await inputView.inputPurchase();
+    const count = CalculatePurchaseCount(purchaseAmount);
+    outputView.printPurchaseCount(count);
+
+    const lottoList = Repeat(count);
+
+    const winningNumber = await inputView.inputWinningNumber();
+    const bonusNumber = await inputView.inputBonusNumber();
+    validateBonusNumber(bonusNumber, winningNumber);
+
+    outputView.printResultTitle();
+
+    const totalMoney = CalculateResultCount(
+      lottoList,
+      winningNumber,
+      bonusNumber
+    );
+    outputView.printProfitRate(totalMoney, purchaseAmount);
+  }
+}
