9c63f17d6753d880a8058425b4a089f6e0cdcfbf
202110861 <dbsalsk2514@naver.com>
2025-10-31 19:33:57 +0900

    refactor : Lotto 파일 경로 변경

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,4 @@
-import Lotto from "../src/Lotto";
+import Lotto from "../src/model/Lotto";
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
@@ -15,4 +15,9 @@ describe("로또 클래스 테스트", () => {
   });
 
   // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+  test("로또 번호는 오름차순으로 정렬하여 보여준다.", () => {
+    const lotto = new Lotto([4, 5, 3, 2, 1, 6]);
+
+    expect(lotto.getNumbers()).toBe("[1, 2, 3, 4, 5, 6]");
+  });
 });
